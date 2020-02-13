617567e1afe80c5d6b391fb05f04fa803b9e66c3
tauan-longaretti <tauanlongaretti@gmail.com>
2020-02-12 23:25:13 -0500

    added test to check if next button renders... Network Error

--- star-wars-app/src/components/StarWarsCharacters.test.js
+++ star-wars-app/src/components/StarWarsCharacters.test.js
@@ -1,1 +1,14 @@
 import React from "react";
+import StarWarsCharacters from "./StarWarsCharacters";
+import { render, fireEvent, act } from "@testing-library/react";
+
+test('renders "success" next button', () => {
+    const { getByText, findByText } = render(<StarWarsCharacters />);
+    console.log(getByText);
+
+    act(() => {
+        fireEvent.click(getByText("Next"));
+    });
+
+    findByText(/next/i);
+});
