83e399e6be947771aea1bd890b6580b9c8d8f4a2
tauan-longaretti <tauanlongaretti@gmail.com>
2020-02-12 23:52:12 -0500

    added test for previous button

---
+++ star-wars-app/src/components/StarWarsCharacters.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import StarWarsCharacters from "./StarWarsCharacters";
+import { render, fireEvent, act } from "@testing-library/react";
+
+test('renders "success" next button', () => {
+    const { getByText, findByText } = render(<StarWarsCharacters />);
+    console.log(getByText);
+
+    act(() => {
+        fireEvent.click(getByText("Next"));
+    });
+
+    findByText(/next/i);
+});
+
+test('renders "success" previous button', () => {
+    const { getByText, findByText } = render(<StarWarsCharacters />);
+    console.log(getByText);
+
+    act(() => {
+        fireEvent.click(getByText("Previous"));
+    });
+
+    findByText(/previous/i);
+});
